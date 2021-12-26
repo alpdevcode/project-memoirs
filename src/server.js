e7bb93061812787f35ac116280da7d2d97378aac
alpdevcode <alp.rgorpilla@gmail.com>
2021-12-26 23:58:27 +0800

    Setup Moralis SDK, Web3 login and logout
    Setup Moralis SDK and initial Web3 login and logout functionality

---
+++ src/server.js
@@ -0,0 +1,18 @@
+// Server URL and App ID are defined in a configuration file
+Moralis.start({ serverUrl, appId });
+
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+        user = await Moralis.authenticate();
+    }
+    console.log("logged in user:", user);
+}
+
+async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
